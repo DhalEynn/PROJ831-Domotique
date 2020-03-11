adf6e8a330d890b948792e99bd1e4e82617ea897
marsuraa <tony2parker0@gmail.com>
2020-03-11 17:17:52 +0100

    Update getData.py
    Add getDate and getITem

--- Transfer/getData.py
+++ Transfer/getData.py
@@ -8,6 +8,40 @@ PASSWORD = os.getenv('DB_PASSWORD')
 SERVER = os.getenv('DB_SERVER')
 
 client = MongoClient('mongodb://' + USERNAME + ':' + PASSWORD + '@' + SERVER)
+
+#db domo
 db = client.domo
+
+#collection logs
 logs = db.logs
+
+#collection analyse
 analyse = db.analyse
+
+
+def getItem(categ,Id):
+    results = []
+    for log in logs.find({"Category": categ, "Id" : Id}):
+        results.append(log)
+    return results
+
+def getDate(period,t1,t2):
+    #mc fait bim bam boom
+    results = []
+    if period =='begin':
+
+        for log in logs.find({'Begin Date': {"$gt" : t1 ,"$lt" :t2}}): 
+            results.append(log)
+        return results
+    
+    elif period =='end':
+
+        for log in logs.find({'Ending Date': {"$gt" : t1 ,"$lt" :t2}}):
+            results.append(log)
+        return results
+    
+    elif period =='full':
+
+        for log in logs.find({'Begin Date': {"$gt" : t1 ,"$lt" :t2},'Ending Date': {"$gt" : t1 ,"$lt" :t2}}):
+            results.append(log)
+        return results
