a622e90399b09b8b4462e1d9457f6d378b106741
marsuraa <tony2parker0@gmail.com>
2020-03-11 14:54:17 +0100

    hash contenu de data

--- sendData.py
+++ sendData.py
@@ -1,6 +1,7 @@
 import os
 
 import pymongo
+import copy
 from pymongo import MongoClient
 
 USERNAME = os.getenv('DB_USERNAME')
@@ -15,12 +16,38 @@ logs = db.logs
 
 analyse = db.analyse
 
+
+def make_hash(o):
+
+  """
+  Makes a hash from a dictionary, list, tuple or set to any level, that contains
+  only other hashable types (including any lists, tuples, sets, and
+  dictionaries).
+  """
+
+  if isinstance(o, (set, tuple, list)):
+
+    return tuple([make_hash(e) for e in o])    
+
+  elif not isinstance(o, dict):
+
+    return hash(o)
+
+  new_o = copy.deepcopy(o)
+  for k, v in new_o.items():
+    new_o[k] = make_hash(v)
+
+  return hash(tuple(frozenset(sorted(new_o.items()))))
+
+
+
 data = {
-    '_id': 1,
     'string': 'Python and MongoDB',
     'list': [1, 2, 3],
     'int': 3
 }
+
+data['_id'] = make_hash(data)
 
 result = logs.insert_one(data)
 print('One logs: {0}'.format(result.inserted_id))
