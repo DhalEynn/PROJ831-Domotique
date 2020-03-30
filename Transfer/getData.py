321e43c3aa46ef306f05fab5883600f41ed38582
Clement <clement.gobe@etu.univ-savoie.fr>
2020-03-30 14:08:46 +0200

    Comment functions

--- Transfer/getData.py
+++ Transfer/getData.py
@@ -1,13 +1,18 @@
-def getItem(categ,Id):
+def getItem(categ, Id):
+    '''
+        Obtenir tous les événements d'un item défini par une Catégorie un ID
+    '''
     results = []
     for log in logs.find({"Category": categ, "Id" : Id}):
         results.append(log)
     return results
 
-def getDate(period,t1,t2):
+def getDate(period, t1, t2):
     '''
-        Si Period = begin(resp. end): Renvoie les events tel que t1 < Begin(resp. Ending) Date < t2
-        Si Perdiod = full: Renvoie les events tel que t1 < Begin Date et End Date < t2
+        Obtenir tous les événements dans un intevalle [t1, t2]
+
+        Si Period = begin(resp. end) -> Renvoie les events tel que t1 < Begin(resp. Ending) Date < t2
+        Si Period = full -> Renvoie les events tel que t1 < Begin Date et End Date < t2
     '''
     results = []
     if period =='begin':
@@ -29,19 +34,28 @@ def getDate(period,t1,t2):
         return results
         
 def getCategory(categ):
+    '''
+        Obtenir tous les événements des item d'une Catégorie
+    '''
     results = []
     for r in logs.find({'Category': categ}):
         results.append(r)
     return results
 
 def getFunction(funct):
+    '''
+        Obtenir tous les événements qui effectue une Function
+    '''
     results = []
     for r in logs.find({'Function': funct}):
         results.append(r)
     return results
 
 def getCommand(comm):
+    '''
+        Obtenir tous les événements qui effectue une Commande
+    '''
     results = []
-    for r in logs.find({'Command': funct}):
+    for r in logs.find({'Command': comm}):
         results.append(r)
     return results
