8b87328017171cde85f03fd2258a4700ecac1b27
Amaury <43411870+AmauryBs@users.noreply.github.com>
2020-04-08 11:55:27 +0200

    prediction

--- Analysis/prediction.py
+++ Analysis/prediction.py
@@ -5,12 +5,19 @@ from statsmodels.tsa.arima_model import ARIMA
 from sklearn.metrics import mean_squared_error
 import Analysis as an
 
+
+import math
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from sklearn.preprocessing import MinMaxScaler
+
 def arimaPrediction():
-    series = pd.Series(an.action_to_list('ROLLINGSHUTTER',5, 10000)[1][:1000])
+    series = pd.Series(an.action_to_list('SWITCH',7, 3000)[1])
     X = series.values
     series.plot()
     pyplot.show()
-    size = int(len(X) * 0.85)
+    size = int(len(X) * 0.60)
     train, test = X[0:size], X[size:len(X)]
     history = [x for x in train]
     predictions = list()
@@ -18,7 +25,7 @@ def arimaPrediction():
         model = ARIMA(history, order=(5,1,0))
         model_fit = model.fit(disp=0)
         output = model_fit.forecast()
-        yhat = output[0]
+        yhat = [round(output[0][0])]
         predictions.append(yhat)
         obs = test[t]
         history.append(obs)
@@ -27,4 +34,7 @@ def arimaPrediction():
     # plot
     pyplot.plot(test)
     pyplot.plot(predictions, color='red')
+    pyplot.show()
+
+    pyplot.plot(test)
     pyplot.show()
