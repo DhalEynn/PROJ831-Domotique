49127a17e7201c1849db561676be728cf0a1d0e8
Amaury <43411870+AmauryBs@users.noreply.github.com>
2020-04-10 12:48:06 +0200

    arima delete et tracé period

--- Analysis/Analysis.py
+++ Analysis/Analysis.py
@@ -8,25 +8,35 @@ import plotly.graph_objects as go
 import pandas as pd
 import seaborn as sns
 from sklearn.metrics import mean_squared_error
+from matplotlib import pyplot
 
 def lastObjectFreq(category,ID, time):
-    logs = connectDB.connectToCollection('logs2')
+    """
+    calcul la frequence d'apparition de tout les objet avant l'action de l'objet donné dans la période time.
+    """
+    logs = connectDB.connectToCollection('logs')
+    # on récupère toute les actions d'un objet
     list_actions = getData.getItem(logs, category, ID, ['EDGE RUNNED'])
     dico_frequency = {}
     nb_actions = len(list_actions)
+    # on parcours les actions 
     for action in list_actions:
+        # on récupère toute les actions précédents notre action
         previous_action = getData.getDate(logs, 'begin', action['Begin Date'], action['Begin Date'] + time, ['EDGE RUNNED'])
         past_action = []
+        # on parcours toutes les actions précédentes
         for p_action in previous_action:
             
             category_action = p_action['Category']
             id_action = p_action['Id']
             comande_action = p_action['Function']
+            #si on a pas déjà rajouté l'action à notre dictionnaire on la rajoute
             if [category_action,id_action] not in past_action:
                 past_action.append([category_action,id_action])
+                #si la clé existe déjà on incrémente le conteur
                 if ( dico_frequency.get(category_action, {}).get(id_action, {}).get(comande_action) ):
                     dico_frequency[category_action][id_action][comande_action] += 1
-
+                #sinon on créer la clé
                 elif category_action  not in dico_frequency.keys():
                     dico_frequency[category_action]={id_action:{comande_action: 1}}
 
@@ -50,9 +60,14 @@ def graphLastObjectFreq(dict_frequency, nb_actions):
     fig = px.bar(x=list_object, y=list_objet_frequency)
     fig.show()
 
+
 def action_to_list(category,ID, maxTime):
+    """
+    créer une liste des etat en fonction du temps, pour un objet données.
+    maxTime sert a limiter la quantité de données à traiter.
+    """
     logs = connectDB.connectToCollection('logs')
-    list_action = getData.getItem(logs, category, ID, ['EDGE RUNNED'])
+    list_action = getData.getItem(logs, category, ID, ['EDGE RUNNED']) #récupére tout les action d'un objet
     time = [0]
     etat = [0]
     i = 0
@@ -69,9 +84,12 @@ def action_to_list(category,ID, maxTime):
             time += [i]
             i += 1
             etat += [int(action['Ending State'][1])]
-    return [time,etat]
+    return [time,etat] #on retourne une liste contenant, une liste de temps et une liste d'état.
 
-def list_to_graph(list_action):         
+def list_to_graph(list_action):
+    """
+    plot the graph of one object (state value according to time)
+    """         
     fig = go.Figure()
     fig.add_trace(go.Scatter(x=list_action[0], y=list_action[1],
                     mode='lines+markers',
@@ -80,10 +98,14 @@ def list_to_graph(list_action):
 
     
     
-    
-def frequence(time,list_etat, transition):
+
+def mean_time(time,list_etat, transition):
+    """
+    calcul le temps moyen qu'une fonction passe dans l'etat on ou off.
+    -1 pour off. 1 pour on.
+    """   
     i = 1
-    time_debut = 0
+    time_debut = -1
     time_fin = 0
     nb = 0
     somme = 0
@@ -92,9 +114,9 @@ def frequence(time,list_etat, transition):
             if  transition == list_etat[i] - list_etat[i-1]:
                 time_debut = time[i]
                 #print("time_debut = ", time_debut)
-            else:
+            elif(time_debut != -1):
                 time_fin = time[i]
-                #print("time_fin = ", time_fin)
+                #print("duree =  ", time_fin - time_debut)
                 somme += (time_fin - time_debut)
                 nb += 1
         i += 1
@@ -102,51 +124,69 @@ def frequence(time,list_etat, transition):
     return(somme/nb) 
 
 
-def period(list_etat):
+def period(list_state):
+    """ 
+    Calculate the period between 2 on state and the period between 2 off state
+    list_state: list with all the states
+    return: the list of all on period and the list of all off period
+    """
     onPeriodList = [] 
     offPeriodList = []
     onPeriod = 0
     offPeriod = 0
-    if list_etat[0] == 1:
+    if list_state[0] == 1:
         onPeriod = 1
     else :
         offPeriod = 1
-    for i in range (len(list_etat)):
-        while i < len(list_etat):
-            if list_etat[i] == 0:
-                if list_etat[i] != list_etat[i-1]:
-                    onPeriodList.append(onPeriod)
-                    onPeriod = 0
-
-                else:
-                    onPeriod+=1
+    # browse through list_state and increment onPeriod and offPeriod when it is needed
+    for i in range (len(list_state)):
+        if list_state[i] == 0:
+            # if the state change, save the value of onPeriod and then set it to 0
+            if list_state[i] != list_state[i-1]:
+                onPeriodList.append(onPeriod)
+                onPeriod = 0
 
             else:
-                if list_etat[i] != list_etat[i-1]:
-                    offPeriodList.append(offPeriod)
-                    offPeriod = 0
-                else:
-                    offPeriod+=1
+                onPeriod+=1
+
+        else:
+            # if the state change, save the value of offPeriod and then set it to 0
+            if list_state[i] != list_state[i-1]:
+                offPeriodList.append(offPeriod)
+                offPeriod = 0
+            else:
+                offPeriod+=1
+    on = pd.Series(onPeriodList)
+    off = pd.Series(offPeriodList)
+    pyplot.plot(on)
+    pyplot.plot(off,color='red')  
+    return(onPeriodList, offPeriodList)
 
 
 
 
 
 
-def correlation():
+def correlation(maxSize):
+    """ 
+    return the correlation matrix of all the objects
+    maxSize: the maximum size of the dataframe, (if it is too high it may create collumns with differents sizes and create a bug) 
+    """
+    # get the logs
     logs = connectDB.connectToCollection('logs')
     categories = getData.getAllExistingCategories(logs)
     data ={}
+    # build a dataframe with each column being a unique category,id couple
     df = pd.DataFrame(data)
-    maxSize = 240000
     for category in categories:
         ids = getData.getAllIdFromCategory(logs,category)
         for id_c in ids:
             a =action_to_list(category,int(id_c),float('inf'))[1]
 
             df[category+ ' ' + str(id_c)]= a[0:maxSize]
-        print(df)
+    # create the correlation matrix
     corr = df.corr()
+    # plot it
     ax = sns.heatmap(
         corr, 
         vmin=-1, vmax=1, center=0,
@@ -160,9 +200,10 @@ def correlation():
     );
 
 
-#f1 = action_to_list('ROLLINGSHUTTER',5, 10000)[1]
+f1 = action_to_list('ROLLINGSHUTTER',5, 10000)
+list_to_graph(f1)
 #f2 = action_to_list('HEATER',6,10000)[1]
-
+#mean_time(f1[0], f1[1], 1)
 #print(correlation())
 
 
--- Analysis/prediction.py
+++ Analysis/prediction.py
@@ -7,40 +7,24 @@ import Analysis as an
 
 
 
-def arimaPrediction():
-    series = pd.Series(an.action_to_list('SWITCH',7, 3000)[1])
-    X = series.values
-    series.plot()
-    pyplot.show()
-    size = int(len(X) * 0.60)
-    train, test = X[0:size], X[size:len(X)]
-    history = [x for x in train]
-    predictions = list()
-    for t in range(len(test)):
-        model = ARIMA(history, order=(5,1,0))
-        model_fit = model.fit(disp=0)
-        output = model_fit.forecast()
-        yhat = [round(output[0][0])]
-        predictions.append(yhat)
-        obs = test[t]
-        history.append(obs)
-    error = mean_squared_error(test, predictions)
-    print('Test MSE: %.3f' % error)
-    # plot
-    pyplot.plot(test)
-    pyplot.plot(predictions, color='red')
-    pyplot.show()
-
+#1440
 def prevision(time, list_etat, duree):
-    periode_off = an.frequence(time,list_etat, -1)
-    periode_on = an.frequence(time,list_etat, 1)
+    """
+    Prend le temps qu'une fonction passe dans l'etat on ou off.
+    Puis continue la fonction, des que le temps passé dans un état dépasse le temps moyen on change d'état et ainsi de suite.
+    duree définis la periode sur laquelle on va faire des prevision.
+    """
+    periode_off = an.mean_time(time,list_etat, -1)
+    periode_on = an.mean_time(time,list_etat, 1)
     i = 1
+    #on récupére le dernier changement d'etat et le temps auquel il ce produit.
     while i<len(list_etat):
         if list_etat[i] != list_etat[i-1]:
             last_transition = list_etat[i] - list_etat[i-1]
             last_time = time[i]
         i += 1
     next_etat=list_etat[-1]
+    #on calcul le temps qu'il reste dans l'etat en cours.
     if last_transition == 1:
         time_to_transition = periode_on - (time[-1] - last_time)
     else:
@@ -50,6 +34,7 @@ def prevision(time, list_etat, duree):
         time += [time[-1] + 1]
         list_etat += [next_etat]
         time_to_transition = time_to_transition -1
+        #on change le prochain etat et on recalcul le temps moyen avant la transition suivant.
         if time_to_transition <= 0:
             if next_etat == 1:
                 next_etat = 0
@@ -59,14 +44,14 @@ def prevision(time, list_etat, duree):
                 time_to_transition = periode_on
         i += 1
     return ([time, list_etat])
-    liste_action = an.action_to_list('LIGHT',1, 5000)
 
 
-liste_action = an.action_to_list('LIGHT',1, 5000)
-an.list_to_graph(liste_action)
-predictions = prevision(liste_action[0], liste_action[1], 10000)
-an.list_to_graph(predictions)
+# liste_action = an.action_to_list('LIGHT',1, 5000)
+# an.list_to_graph(liste_action)
+# predictions = prevision(liste_action[0], liste_action[1], 10000)
+# an.list_to_graph(predictions)
 
-liste_action = an.action_to_list('LIGHT',1, 15000)
-an.list_to_graph(liste_action)
-mean_squared_error(liste_action, predictions)
+# liste_action = an.action_to_list('LIGHT',1, 15000)
+# an.list_to_graph(liste_action)
+# mean_squared_error(liste_action, predictions)
+
