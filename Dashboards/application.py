b071a1c7b2efd713d85eafa69bf959427e0488e0
Clement <clement.gobe@etu.univ-savoie.fr>
2020-04-10 11:07:48 +0200

    Update chart

--- Dashboards/application.py
+++ Dashboards/application.py
@@ -11,7 +11,6 @@ from flask import request
 
 import plotly
 import plotly.graph_objs as go
-import numpy as np
 import json
 
 
@@ -36,12 +35,11 @@ def logs():
     actions = getData.getAllExistingActions(logs)
     # selection with button
     if request.method == 'POST':
+        chart = None
         req = request.form['item']
         # all
-
         if req == 'all':
             events = getData.getAll(logs, 100,True)
-            bar = None
         # item
         else: 
             item = req.split()
@@ -50,28 +48,25 @@ def logs():
             else:
                 item[1] = int(item[1])
                 events = getData.getItem(logs, item[0], item[1] , actions, 100,True)
-
-            # create plot
-            x = []
-            y = []
-            for event in events:
-                if event['Action'] == 'EDGE RUNNED':
-                    x.append(event['Begin Date'])
-                    if event['Command'] in ['ON', 'UP']:
-                        y.append(1)
-                    elif event['Command'] in ['OFF', 'DOWN']:
-                        y.append(0)
-                    else:
-                        y.append(-1)
-            data = [go.Bar(x=x, y=y)]
-            bar = json.dumps(data, cls=plotly.utils.PlotlyJSONEncoder)
-
+                # create plot
+                x = []
+                y = []
+                for event in events:
+                    if event['Action'] == 'EDGE RUNNED':
+                        x.append(event['Begin Date'])
+                        if event['Command'] in ['ON', 'UP']:
+                            y.append(1)
+                        elif event['Command'] in ['OFF', 'DOWN']:
+                            y.append(0)
+                        else:
+                            y.append(-1)
+                fig = [go.Bar(x=x, y=y)]    
+                chart = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
     # default page
     else:
         events = getData.getAll(logs, 100,True)
-        bar = None
 
-    return render_template("logs.html", items=items, nb_line=nb_line, events=events, plot=bar)
+    return render_template("logs.html", items=items, nb_line=nb_line, events=events, plot=chart)
 
 @app.route("/analyses")
 def analyse():
