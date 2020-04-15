14fd95b312f7c77fb5a39f51b70f9ddd608e6124
DahlEynn <DahlEynn@users.noreply.github.com>
2020-04-15 14:56:18 +0200

    Update sendData (adding percent + time counters)

--- Transfer/sendData.py
+++ Transfer/sendData.py
@@ -1,3 +1,21 @@
+import time
+from datetime import timedelta
+
 def send_items(items, collection):
+	start_time = time.time()
+	print("There are %d items to send." % (len(items)))
+	size_percent = int(len(items) * 0.01)
+	value = 0
+	percent = 0
+	temp_time = start_time
 	for item in items:
+		if (value == size_percent):
+			percent += 1
+			print("We are now at %d%%" % (percent))
+			print("Elapsed real time : %s" % str(timedelta(seconds = (time.time() - temp_time))))
+			temp_time = time.time()
+			value = 0
+		else:
+			value += 1
 		collection.update_one(item, {'$set': item}, upsert=True)
+	print("Elapsed total time : %s." % str(timedelta(seconds = (time.time() - start_time))))
