5180572c1caa96d1d03830d8ae102e8bc1c3ab4e
Clement <clement.gobe@etu.univ-savoie.fr>
2020-05-12 14:45:11 +0200

    Prediction graph (wip)

--- Analysis/generate.py
+++ Analysis/generate.py
@@ -4,6 +4,7 @@ import env
 import Transfer.connectDB as connectDB
 import Transfer.getData as getData
 import Analysis.Analysis as analysis
+import Analysis.prediction as prediction
 import json
 import plotly
 
@@ -28,3 +29,23 @@ def createGraphLastObjectFreq(items):
                     'jsondumps': json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)}
             charts.append(chart)
     return charts
+
+def createPredictions(items):
+    charts = []
+    for category in items:
+        for Id in items[category]:
+            data = analysis.action_to_list(category, Id, 5000)
+            # prediction 1
+            predictions = prediction.prediction(data[0], data[1], 10000)
+            fig = analysis.list_to_graph(predictions)
+            # prediction 2
+            # list_freq = an.frequency_activation_minute(liste_action, 5000, 1440)
+            # predictions = periodPrediction(liste_action, 10000, list_freq, 1440)
+            # fig = analysis.list_to_graph(predictions)
+            chart = {'Category': category,
+                    'Id':  Id,
+                    'chart_type': 'prediction',
+                    'jsondumps': json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)}
+            charts.append(chart)
+    return charts
+
